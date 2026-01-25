eb9249b2f83c3d875ae2f30480bf1d5cba3731e4
Brun0Silveir4 <bruno.silveira.dionisio@gmail.com>
2026-01-24 22:31:48 -0300

    fix: implemented updatePassword route with ensureAuth middleware

--- src/Routes/login-route.js
+++ src/Routes/login-route.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/register", UserController.createUser)
 router.post("/login", UserController.LoginUser)
 router.put("/update/me", ensureAuth, UserController.updateUser)
+router.put("/update/me/password", ensureAuth, UserController.updatePassword)
 
 module.exports = router
